9252d64364666450893c89597432560510665fb4
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-11-09 13:58:24 -0600

    formatting and date

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -86,18 +86,21 @@ export default function home({ posts }) {
                 key={post._id}
               >
                 {post.title}
+                {Date(post.createdAt)}
               </div>
             );
           })
         : null}
       <div>
         <form className="flex flex-col" onSubmit={handleFormSubmit}>
-          <div className="flex flex-col bg-blue-300 justify-center">
-            <div className="m-2 space-y-4 w-2/3">
-              <div>
-                <label for="title">Title</label>
+          <div className="flex flex-col bg-blue-300 justify-center items-center">
+            <div className="m-2 space-y-4 w-[400px]">
+              <div className="flex">
+                <label className="w-[100px] text-center" for="title">
+                  Title
+                </label>
                 <input
-                  className="w-1/4"
+                  className="w-2/3"
                   id="title"
                   type="text"
                   value={title}
@@ -105,10 +108,12 @@ export default function home({ posts }) {
                   onChange={(e) => setTitle(e.target.value)}
                 />
               </div>
-              <div>
-                <label for="content">Content</label>
+              <div className="flex">
+                <label className="w-[100px] text-center" for="content">
+                  Content
+                </label>
                 <textarea
-                  className="w-1/4"
+                  className="w-2/3"
                   id="content"
                   type="text"
                   required
@@ -116,12 +121,14 @@ export default function home({ posts }) {
                   onChange={(e) => setContent(e.target.value)}
                 />
               </div>
-              <div>
-                <label for="category">Category</label>
+              <div className="flex">
+                <label className="w-[100px] text-center" for="category">
+                  Category
+                </label>
                 <select
                   value={category}
                   id="category"
-                  className="w-1/4"
+                  className="w-2/3"
                   onChange={(e) => {
                     setCategory(e.target.value);
                   }}
@@ -134,20 +141,25 @@ export default function home({ posts }) {
                   <option value="Cardio">Sport</option>
                 </select>
               </div>
-              <div>
-                <label for="duration">Total Duration - min</label>
+              <div className="flex">
+                <label className="w-[100px] text-center" for="duration">
+                  Duration
+                </label>
                 <input
                   value={duration}
-                  className="w-1/4"
+                  className="w-2/3"
                   id="duration"
                   type="number"
                   min="1"
                   onChange={(e) => setDuration(e.target.value)}
                 />
               </div>
-              <div>
-                <label for="exercise">Exercise</label>
+              <div className="flex">
+                <label className="w-[100px] text-center" for="exercise">
+                  Exercise
+                </label>
                 <input
+                  className="w-2/3"
                   value={exercise.name}
                   type="text"
                   onChange={(e) =>
@@ -155,50 +167,52 @@ export default function home({ posts }) {
                   }
                 />
               </div>
-            </div>
-            {category == "Weight Training" ? (
-              <div className="p-2 flex w-2/5 justify-center">
-                <div className="">
-                  <label for="sets">Sets</label>
-                  <input
-                    className="ml-2 w-1/3"
-                    id="sets"
-                    type="number"
-                    onChange={(e) =>
-                      setExercise({ ...exercise, sets: e.target.value })
-                    }
-                  />
+              {category == "Weight Training" ? (
+                <div className="p-2 flex w-full justify-center">
+                  <div className="">
+                    <label for="sets">Sets</label>
+                    <input
+                      className="ml-2 w-1/3"
+                      id="sets"
+                      type="number"
+                      onChange={(e) =>
+                        setExercise({ ...exercise, sets: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div>
+                    <label for="reps">Reps</label>
+                    <input
+                      className="ml-2 w-1/3"
+                      id="reps"
+                      type="number"
+                      onChange={(e) =>
+                        setExercise({ ...exercise, reps: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div>
+                    <label for="weight">Weight - lbs</label>
+                    <input
+                      className="ml-2 w-1/3"
+                      id="weight"
+                      type="number"
+                      onChange={(e) =>
+                        setExercise({ ...exercise, weight: e.target.value })
+                      }
+                    />
+                  </div>
                 </div>
-                <div>
-                  <label for="reps">Reps</label>
-                  <input
-                    className="w-1/3"
-                    id="reps"
-                    type="number"
-                    onChange={(e) =>
-                      setExercise({ ...exercise, reps: e.target.value })
-                    }
-                  />
-                </div>
-                <div>
-                  <label for="weight">Weight - lbs</label>
-                  <input
-                    className="w-1/3"
-                    id="weight"
-                    type="number"
-                    onChange={(e) =>
-                      setExercise({ ...exercise, weight: e.target.value })
-                    }
-                  />
-                </div>
+              ) : null}
+              <div className="w-full flex justify-center items-center">
+                <button
+                  className="border border-black w-[100px]"
+                  onClick={handleAddExercise}
+                >
+                  add exercise
+                </button>
               </div>
-            ) : null}
-            <button
-              className="border border-black w-[100px]"
-              onClick={handleAddExercise}
-            >
-              add exercise
-            </button>
+            </div>
           </div>
           {exerciseList
             ? exerciseList.map((exercise) => {
