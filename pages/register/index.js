9525cb997cad9d21d3b537f43122da46b0ffc8b5
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-12-16 16:49:51 -0600

    valid username
    only alphanumerical for username. green border if valid.

--- pages/register/index.js
+++ pages/register/index.js
@@ -2,28 +2,45 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function register() {
-  const [username, setUsername] = useState();
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState();
   const [error, setError] = useState();
   const [pwStrength, setPwStrength] = useState();
-  let weakPW = new RegExp("[a-zA-Z0-9]");
-  let pwRegex = new RegExp("(?=.*[a-zA-Z])(?=.*[0-9])");
-  let pwRegex2 = new RegExp(
+  const [validUsername, setValidUsername] = useState(false);
+  const [usernameHint, setUsernameHint] = useState(false);
+
+  let alphaNum = new RegExp("[a-zA-Z0-9]");
+  let alphaNumCombined = new RegExp("(?=.*[a-zA-Z])(?=.*[0-9])");
+  let alphaNumCombinedSpecial = new RegExp(
     "^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$"
   );
 
+  let userRegex = new RegExp("[^A-Za-z 0-9]");
+
   useEffect(() => {
-    if (pwRegex2.test(password)) {
+    if (alphaNumCombinedSpecial.test(password)) {
       setPwStrength("strong");
-    } else if (pwRegex.test(password)) {
+    } else if (alphaNumCombined.test(password)) {
       setPwStrength("medium");
-    } else if (weakPW.test(password) && password !== "") {
+    } else if (alphaNum.test(password) && password !== "") {
       setPwStrength("weak");
     } else {
       setPwStrength("");
     }
   }, [password]);
+
+  useEffect(() => {
+    if (!userRegex.test(username) && username !== "" && username.length >= 3) {
+      setValidUsername(true);
+    } else {
+      setValidUsername(false);
+    }
+  }, [username]);
+
+  useEffect(() => {
+    console.log(validUsername);
+  });
 
   // if (pwRegex2.test(password)) {
   //   setPwStrength("strong");
@@ -67,7 +84,6 @@ export default function register() {
       }
     } catch (error) {
       console.log(error.message);
-      // setError(error.message);
     }
   }
 
@@ -75,32 +91,67 @@ export default function register() {
     <>
       <div className="flex flex-col justify-center items-center h-screen">
         <div className="my-2">Register</div>
-        <div className="py-4 w-[400px] bg-slate-300 rounded-lg flex flex-col justify-center items-center">
-          <form onSubmit={handleFormSubmit} className="flex flex-col space-y-2">
-            <label for="username">Username</label>
-            <input
-              required
-              id="username"
-              className="border border-black"
-              type="text"
-              value={username}
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-            />
-            <label for="password">Password</label>
-            <input
-              required
-              maxlength="16"
-              id="password"
-              className="border border-black"
-              type="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-            <div className="w-full bg-white h-[10px] rounded-lg">
+        <div className="relative py-4 w-[400px] h-[400px] bg-slate-300 rounded-lg flex flex-col justify-center items-center">
+          <form
+            onSubmit={handleFormSubmit}
+            className="flex flex-col space-y-4 w-3/4 h-full items-center justify-center"
+          >
+            <div className="flex flex-col justify-center items-start">
+              <div className="flex">
+                <label for="username">Username</label>
+              </div>
+              <input
+                required
+                onKeyDown={(e) => {
+                  if (e.key === " ") {
+                    e.preventDefault();
+                  }
+                }}
+                onFocus={() => {
+                  setUsernameHint(true);
+                }}
+                onBlur={() => {
+                  setUsernameHint(false);
+                }}
+                id="username"
+                maxlength="16"
+                className={
+                  validUsername
+                    ? "border border-green-300 w-[200px]"
+                    : "border border-black w-[200px]"
+                }
+                type="text"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
+              <div className="absolute bottom-0 text-[10px] bg-red-300">
+                {usernameHint
+                  ? "At least 3 characters, letters and numbers only"
+                  : null}
+              </div>
+            </div>
+            <div className="flex flex-col justify-center items-start">
+              <label for="password">Password</label>
+              <input
+                onKeyDown={(e) => {
+                  if (e.key === " ") {
+                    e.preventDefault();
+                  }
+                }}
+                required
+                maxlength="16"
+                id="password"
+                className="border border-black w-[200px]"
+                type="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div className="w-[200px] bg-white h-[10px] rounded-lg">
               <div
                 className={
                   pwStrength == "strong"
@@ -125,9 +176,14 @@ export default function register() {
             <div className="mt-2 flex flex-col">
               <label for="email">Email</label>
               <input
+                onKeyDown={(e) => {
+                  if (e.key === " ") {
+                    e.preventDefault();
+                  }
+                }}
                 required
                 id="email"
-                className="border border-black"
+                className="border border-black w-[200px]"
                 type="email"
                 value={email}
                 placeholder="hendev@yahoo.com"
