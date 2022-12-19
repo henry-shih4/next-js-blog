f7434fdc0f6d417a96e44aab1b67a233d8e1d645
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-12-19 12:58:56 -0600

    Update [id].js
    take out pw in response

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -5,7 +5,6 @@ export default async function handler(request, response) {
   const { db } = await connectToDatabase();
   const { method } = request;
   const { id } = request.query;
-
   if (method === "GET") {
     const user = await db
       .collection("users")
@@ -14,7 +13,16 @@ export default async function handler(request, response) {
     if (!user) {
       return response.status(400).json("user not found");
     }
-    return response.status(200).json(user);
+    return response
+      .status(200)
+      .json({
+        username: user.username,
+        email: user.email,
+        photoURL: user.photoURL,
+        userId: user.userId,
+        createdAt: user.createdAt,
+        numPosts: user.numPosts,
+      });
   }
 
   if (method === "PUT") {
